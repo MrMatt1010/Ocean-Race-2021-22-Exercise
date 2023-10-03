f7df4e95e85a1de8ab32b8193651c8cef2facd3a
Matt T <mrsymtec@gmail.com>
2023-10-03 17:06:44 +1300

    Add project files.

---
+++ Ocean Race 2021-22/Ocean Race 2021-22.cpp
@@ -0,0 +1,37 @@
+// Ocean Race 2021-22.cpp : 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Location {
+
+private:
+
+    int degreesLat;
+
+    float minutesLat;
+
+    char directionLat; //This is normally North or South for latitude
+
+    int degreesLong;
+
+    float minutesLong;
+
+    char directionLong; //This is normally West or East for longitude 
+
+
+public:
+
+    //This is where the constructor sits to initialize the location object
+    location() : degreesLat(0), minutesLat(0), 
+
+
+};
+
+int main()
+{
+    
+}
+
